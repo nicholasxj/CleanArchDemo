00a90e8f0aca93e1284977b36bc0b1967151e6b4
Nicholas Jakubowicz <89029759+nicholasjakubowicz@users.noreply.github.com>
2022-11-25 10:21:24 -0500

    Commit Service and Repository Interfaces, CourseViewModel

---
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,14 @@
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+
+    }
+}
