aac5bee11e4b2999c6a496b86e943f217231865e
Nicholas Jakubowicz <89029759+nicholasjakubowicz@users.noreply.github.com>
2022-11-25 10:39:06 -0500

    Commit implementation of Service and Repository GetCourses

---
+++ CleanArch/CleanArch.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Domain.Models;
+using CleanArch.Domain.Models.Interfaces;
+using CleanArch.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infrastructure.Data.Repository
+{
+	public class CourseRepository : ICourseRepository
+	{
+		private readonly UniversityDbContext _context;
+
+		public CourseRepository(UniversityDbContext context)
+		{
+			_context = context;
+		}
+
+		public IEnumerable<Course> GetCourses()
+		{
+			return _context.Courses;
+		}
+	}
+}
