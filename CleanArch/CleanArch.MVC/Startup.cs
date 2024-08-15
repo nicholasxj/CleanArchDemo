ea914e50a5415193752efd87a81cac669158a071
Nicholas Jakubowicz <89029759+nicholasjakubowicz@users.noreply.github.com>
2024-08-15 14:26:16 -0400

    updated the startup file to allow un-verified users.

--- CleanArch/CleanArch.MVC/Startup.cs
+++ CleanArch/CleanArch.MVC/Startup.cs
@@ -34,7 +34,7 @@ namespace CleanArch.MVC
 					Configuration.GetConnectionString("UniversityIdentityDbConnection")));
 			services.AddDatabaseDeveloperPageExceptionFilter();
 
-			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddControllersWithViews();
 
