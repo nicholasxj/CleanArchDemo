12e10f8b7b12eb10fb338d0783c319020411e947
Nicholas Jakubowicz <89029759+nicholasjakubowicz@users.noreply.github.com>
2024-08-16 15:32:58 -0400

    Added CourseService Create functionnality. Updated CourseViewModel.

--- CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -8,6 +8,10 @@ namespace CleanArch.Application.ViewModels
 {
     public class CourseViewModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
         public IEnumerable<Course> Courses { get; set; }
 
     }
